74296b097cf2ab99f08d4889d682effddfe7bd41
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2022-10-05 23:51:26 +0900

    뷰 템플릿과 MVC 패턴

---
+++ src/main/java/com/example/springboot/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.example.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //동료 선언
+public class FirstController {
+
+    @GetMapping("/hi") //URL 요청 연결(필수!!)
+    //메소드를 생성 ->이 메소드를 통해서 view templates를 반환 할 수 있게 해줌
+    public String nice(Model model){
+        model.addAttribute("username","yuchan");
+        return "greeting"; //templates 안에 greetings 파일을 찾아서 -> 브라우저로 전송
+    }
+}
---
+++ src/theory_이론 정리/뷰_템플릿과_MVC패턴.md
@@ -0,0 +1,12 @@
+# 뷰 템플릿과 MVC 패턴
+```
+spring initializr에서 Mustache라는 기능을 추가하였는데 이 도구가 View Templates를 
+만드는 도구이다.
+
+View Templates에는 Controller 와 Model이라는 동료가 존재하며
+Controller(logic)는 처리 과정을 담당하고 Model(data)은 data를 관리합니다.
+
+이렇게 화면,처리,데이터 분야를 각 담당자 별로 나누는 기법을 MVC 패턴이라고 한다.
+
+mustache는 html을 다르게 사용하는 기능(doc 탭 사용하기) 
+```
